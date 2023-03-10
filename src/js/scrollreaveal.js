6c60ed483b4c5bedefefa3f8fac9dbc3eb635f72
Alef Soares <alefcard.soares@gmail.com>
2023-03-10 18:28:18 -0300

    update Scroll Reveal

--- src/js/scrollreaveal.js
+++ src/js/scrollreaveal.js
@@ -2,7 +2,7 @@ window.sr = ScrollReveal({
     reset: true,
     distance: "100px",
     duration: 2500,
-    delay: 400       
+    delay: 400,      
 });
 
 sr.reveal('.home-right .h1', { 
@@ -10,17 +10,41 @@ sr.reveal('.home-right .h1', {
 });
 
 sr.reveal('.home-right .paragraph', { 
-    delay: 350, origin:'left'
+    delay: 370, origin:'left'
 });
 
 sr.reveal('.home-right .home-btn', { 
-    delay: 400, origin:'left'
+    delay: 420, origin:'left'
 });
 
 sr.reveal('.home-left img', { 
-    delay: 450, origin:'right'
+    delay: 470, origin:'right'
 });
 
 sr.reveal('.info-container', { 
     delay: 200, distance: '20px', origin:'bottom'
 });
+
+sr.reveal('.about-top, .about-bottom, .services-top, .tech-top, .how-to-top', { 
+    origin: "top",
+    distance: "30px",
+    duration: 2500,
+    delay: 100,     
+});
+
+sr.reveal('.simulation',{delay: 300, distance: '20px', origin:'bottom'})
+sr.reveal('.education',{delay: 400, distance: '20px', origin:'bottom'})
+sr.reveal('.self-care',{delay: 500, distance: '20px', origin:'bottom'})
+sr.reveal('.outdoor',{delay: 600, distance: '20px', origin:'bottom'})
+
+sr.reveal('.conception-design',{delay: 300, distance: '20px', origin:'bottom'})
+sr.reveal('.interaction-design',{delay: 400, distance: '20px', origin:'bottom'})
+sr.reveal('.vr-world-user-testing',{delay: 500, distance: '20px', origin:'bottom'})
+sr.reveal('.hydra-vr-deploy',{delay: 600, distance: '20px', origin:'bottom'})
+
+sr.reveal('.bxl-facebook-circle',{delay: 300, distance: '20px', origin:'bottom'})
+sr.reveal('.bxl-twitter',{delay: 400, distance: '20px', origin:'bottom'})
+sr.reveal('.bxl-linkedin',{delay: 500, distance: '20px', origin:'bottom'})
+sr.reveal('.bxl-youtube',{delay: 600, distance: '20px', origin:'bottom'})
+sr.reveal('.bxl-instagram',{delay: 700, distance: '20px', origin:'bottom'})
+sr.reveal('.bxl-pinterest',{delay: 800, distance: '20px', origin:'bottom'})
