e725f44cedc7e8b0215181d0dc941a3bdcf35982
Alef Soares <alefcard.soares@gmail.com>
2023-03-09 21:54:01 -0300

    trantanto os bugs e iniciando o ScrollReveal

--- src/js/scrollreaveal.js
+++ src/js/scrollreaveal.js
@@ -1,24 +1,24 @@
 window.sr = ScrollReveal({
     reset: true,
-    distance: "40px",
+    distance: "100px",
     duration: 2500,
     delay: 400       
 });
 
 sr.reveal('.home-right .h1', { 
-    delay: 500, origin:'left'
+    delay: 300, origin:'left'
 });
 
 sr.reveal('.home-right .paragraph', { 
-    delay: 600, origin:'left'
+    delay: 350, origin:'left'
 });
 
 sr.reveal('.home-right .home-btn', { 
-    delay: 700, origin:'left'
+    delay: 400, origin:'left'
 });
 
 sr.reveal('.home-left img', { 
-    delay: 700, origin:'right'
+    delay: 450, origin:'right'
 });
 
 sr.reveal('.info-container', { 
